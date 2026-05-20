359b18eb984447741e68f23bd2ac5ad801639d18
Ragamaie-Nagineni <ragamaie.n@gmail.com>
2026-05-20 16:19:09 +0530

    connected to db

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,26 @@
 import express from "express";
 import cors from "cors";
-
+import {Pool} from "pg";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app=express();
 const port=3000;
 app.use(cors());
+
+const pool=new Pool(
+    {
+        connectionString:process.env.DATABASE_URL,
+        ssl:{
+           rejectUnauthorized:false,
+        }
+    }
+)
+
+pool.query("SELECT NOW()",(err,res)=>{
+     if(err){console.log("DB connection failed!");console.error(err);}
+     else{console.log("DB Connected!");console.log(res.rows);}
+})
 
 app.get("/",(req,res)=>{
     res.json({message:"hello from backend"});
