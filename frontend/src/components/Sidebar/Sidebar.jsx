41c231f393896be5e3539cb23b578d5f3636680e
Ragamaie-Nagineni <ragamaie.n@gmail.com>
2026-05-22 18:41:07 +0530

    added collapse button

--- frontend/src/components/Sidebar/Sidebar.jsx
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -1,4 +1,5 @@
 import react from "react";
+import { useState,useEffect } from "react";
 import "./Sidebar.css";
 import {
   Home,
@@ -9,20 +10,30 @@ import {
   BookOpen,
   Settings,
   LogOut,
+ PanelLeftClose,
+ PanelLeftOpen,
+ Menu
 } from "lucide-react";
 function Sidebar(){
+   const [collapsed,setcollapsed]=useState("");
+   
+   function handleCollapse(){
+      setcollapsed(!collapsed);
+   }
     return(
-       <div className="sidebar">
+       <div className={collapsed ? "sidebar collapsed" : "sidebar"}>
+         <div className="close-btn" onClick={handleCollapse}><Menu/></div>
         <ul>
-           <li className="active"> <Home/>Dashboard</li>
-           <li><FolderGit2/>Repositories</li>
-           <li><MessageSquare />AI Chat</li>
-           <li><Search/>Search</li>
-           <li><BookOpen></BookOpen>Blogs</li>
-           <li><Settings/>Settings</li>
+           <li className="active"> <Home/>{!collapsed && <span>Dashboard</span>}</li>
+           <li><FolderGit2/>{!collapsed && <span>Repositories</span>}</li>
+           <li><MessageSquare />{!collapsed &&<span>AI Chat</span>}</li>
+           <li><Search/>{!collapsed &&<span>Search</span>}</li>
+           <li><BookOpen/>{!collapsed &&<span>Blogs</span>}</li>
+           <li><Settings/>{!collapsed &&<span>Settings</span>}</li>
         </ul>
         <div className="logout">
-           <LogOut/>Log Out
+         <LogOut/>
+           {!collapsed && <span>Log Out</span>}
         </div>
        </div>
     )
